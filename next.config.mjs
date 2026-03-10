91da1114301aaf0a3dc75021242cc99a34f05b4b
automic1324xxy-burner <dicelovebeatbox@gmail.com>
2026-03-10 12:14:15 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  // Replace 'jamiehwangblacksite' with your exact repository name
+  basePath: '/jamiehwangblacksite', 
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
