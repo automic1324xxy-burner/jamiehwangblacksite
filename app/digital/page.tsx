8570e50c147f369dce37c8f02ad308085bd1249a
automic1324xxy-burner <dicelovebeatbox@gmail.com>
2026-03-11 02:05:07 +0200

    Update page.tsx

--- app/digital/page.tsx
+++ app/digital/page.tsx
@@ -39,32 +39,21 @@ export default function Digital() {
 
       <section className="faces-section">
         <div className="faces-scroll">
-          {/* 2. Don't hardcode the repo name in the src. 
-              Next.js adds it automatically if basePath is in next.config.mjs.
-              Also, removed the double // */}
-          <Image src="/jamiehwangblacksite/faces/face1.PNG" alt="Face 1" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face2.PNG" alt="Face 2" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face3.PNG" alt="Face 3" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face4.PNG" alt="Face 4" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face5.PNG" alt="Face 5" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face6.PNG" alt="Face 6" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face7.PNG" alt="Face 7" width={200} height={200} />
-          {/* Duplicates for loop */}
-          <Image src="/jamiehwangblacksite/faces/face1.PNG" alt="Face 1" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face2.PNG" alt="Face 2" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face3.PNG" alt="Face 3" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face4.PNG" alt="Face 4" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face5.PNG" alt="Face 5" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face6.PNG" alt="Face 6" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face7.PNG" alt="Face 7" width={200} height={200} />
-          {/* Duplicates for loop */}
-          <Image src="/jamiehwangblacksite/faces/face1.PNG" alt="Face 1" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face2.PNG" alt="Face 2" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face3.PNG" alt="Face 3" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face4.PNG" alt="Face 4" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face5.PNG" alt="Face 5" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face6.PNG" alt="Face 6" width={200} height={200} />
-          <Image src="/jamiehwangblacksite/faces/face7.PNG" alt="Face 7" width={200} height={200} />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face1.PNG-ol3JOxkaXcXkxrBbrq181kqaJxWw8f.png" alt="Face 1" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face2.PNG-AfIpJO7oeIDNCoM0GYRT4kUcEx7oVU.png" alt="Face 2" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face3.PNG-g2oNj3XtvKJ20JuLEfkywklsGQs3E5.png" alt="Face 3" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face4.PNG-H4w4ogSBCwxPMXZesf9NX2fBF2HvvF.png" alt="Face 4" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face5.PNG-90ZfsGK9A0bvG92ae40lVCRM5j52zE.png" alt="Face 5" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face6.PNG-Bn3CMDpnV6h6wimsYgApQME3hgS8dL.png" alt="Face 6" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face7.PNG-IVD1Df71n1PFXNhDCAT7ZwwWefGlgy.png" alt="Face 7" />
+          {/* Duplicate faces for seamless loop */}
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face1.PNG-ol3JOxkaXcXkxrBbrq181kqaJxWw8f.png" alt="Face 1" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face2.PNG-AfIpJO7oeIDNCoM0GYRT4kUcEx7oVU.png" alt="Face 2" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face3.PNG-g2oNj3XtvKJ20JuLEfkywklsGQs3E5.png" alt="Face 3" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face4.PNG-H4w4ogSBCwxPMXZesf9NX2fBF2HvvF.png" alt="Face 4" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face5.PNG-90ZfsGK9A0bvG92ae40lVCRM5j52zE.png" alt="Face 5" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face6.PNG-Bn3CMDpnV6h6wimsYgApQME3hgS8dL.png" alt="Face 6" />
+          <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/face7.PNG-IVD1Df71n1PFXNhDCAT7ZwwWefGlgy.png" alt="Face 7" />
         </div>
       </section>
       
